b586fbc234687df49bbcf4e309c628639bb9fa81
naginenihari <naginenihari@gmail.com>
2025-09-26 18:38:18 +0530

    payment.sh file added

--- payment.sh
+++ payment.sh
@@ -7,26 +7,8 @@ check_root
 app_setup
 python_setup
 systemd_setup
-
-cp $SCRIPT_DIR/payment.service /etc/systemd/system/payment.service
-systemctl daemon-reload
-systemctl enable payment  &>>$LOG_FILE
-ystemctl restart payment
-
+app_restart
 print_total_time
 
 
 
-systemd_setup(){
-cp $SCRIPT_DIR/$app_name.service /etc/systemd/system/$app_name.service &>>$LOG_FILE
-VALIDATE $? "copy systemctl service"
-systemctl daemon-reload
-systemctl enable $app_name &>>$LOG_FILE
-VALIDATE $? "Enable $app_name Service"
-}
-
-cp $SCRIPT_DIR/payment.service /etc/systemd/system/payment.service
-systemctl daemon-reload
-systemctl enable payment  &>>$LOG_FILE
-
-systemctl restart payment
