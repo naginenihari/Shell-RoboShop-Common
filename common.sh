4263a869e98d7ac977795e3051f0521fb3c08301
naginenihari <naginenihari@gmail.com>
2025-09-26 15:24:29 +0530

    common.sh and mongodb.sh files are created

---
+++ common.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[37m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$(echo $0 |cut -d '.' -f1)
+MONGODB_HOST=mongodb.naginenihariaws.store
+SCRIPT_DIR=$PWD
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+START_TIME=$(date +%s)
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at:$(date)" |tee -a $LOG_FILE
+
+check_root(){
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: please run the script with root privelege" |tee -a $LOG_FILE
+    exit 1  #failure mean other then Zero
+fi
+}
+
+VALIDATE() {
+if [ $1 -ne 0 ]; then
+    echo -e " $2 is $R FAILURE $N" |tee -a $LOG_FILE
+    exit 1
+else 
+    echo -e " $2 is $G SUCCESS $N" |tee -a $LOG_FILE
+fi
+}
+
+print_total_time(){
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
+
+}
---
+++ mongodb.sh
@@ -0,0 +1,25 @@
+#!/nin/bash
+
+source ./common.sh
+
+check_root
+
+cp $SCRIPT_DIR/mongo.repo  /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
+VALIDATE $? "Adding Mongo repo"
+
+dnf install mongodb-org -y &>>$LOG_FILE
+VALIDATE $? "Installing MongoDB"
+
+systemctl enable mongod &>>$LOG_FILE
+VALIDATE $? "Enabled MondoDB"
+
+systemctl start mongod &>>$LOG_FILE
+VALIDATE $? "Started MongoDB"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connections to mongodb"
+
+systemctl restart mongod
+VALIDATE $? "Restarted mongoDB"
+
+print_total_time
